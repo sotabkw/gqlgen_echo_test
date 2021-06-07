c29bc948871c2b03ad6260bff71e1bf310027836
Sota <watanabeso@crunchtimer.jp>
2021-06-08 02:31:24 +0900

    add:main.go追加

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/watanabe-sota/gqlgen_echo_test/graph"
+	"github.com/watanabe-sota/gqlgen_echo_test/graph/generated"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	db, err := gorm.Open(
+		"postgres",
+		fmt.Sprintf(
+			"host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+			"127.0.0.1", 5432, "postgres", "gqlgen-echo-sample", os.Getenv("POSTGRESQL_ROOT_PASSWORD"),
+		),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", welcome())
+
+	graphqlHandler := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: &graph.Resolver{DB: db}},
+		),
+	)
+	playgroundHandler := playground.Handler("GraphQL", "/query")
+
+	e.POST("/query", func(c echo.Context) error {
+		graphqlHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		playgroundHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	err = e.Start(":3000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func welcome() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome!")
+	}
+}
